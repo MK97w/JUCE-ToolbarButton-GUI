cf39014e01eb477ba61b0422d62b32abc328f70a
MERT KABUKÇUOĞLU <kabukcuoglumert@gmail.com>
2023-07-22 00:59:11 +0300

    Initial Commit - Summer background of size 1280x800 is added

---
+++ Source/MainComponent.cpp
@@ -0,0 +1,30 @@
+#include "MainComponent.h"
+
+//==============================================================================
+MainComponent::MainComponent()
+{
+    setSize (1280, 800);
+}
+
+MainComponent::~MainComponent()
+{
+}
+
+//==============================================================================
+void MainComponent::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    g.setFont (juce::Font (16.0f));
+    g.setColour (juce::Colours::white);
+    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
+    g.drawImage(juce::ImageCache::getFromMemory(BinaryData::summer_jpg, BinaryData::summer_jpgSize),getLocalBounds().toFloat());
+}
+
+void MainComponent::resized()
+{
+    // This is called when the MainComponent is resized.
+    // If you add any child components, this is where you should
+    // update their positions.
+}
